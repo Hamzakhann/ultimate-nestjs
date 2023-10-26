15062345fdef367a8a65934a681c19d13b168908
Hamza Khan <hamzakhann66@gmail.com>
2023-10-27 01:00:06 +0500

    added migrations

--- car-price/src/reports/reports.controller.ts
+++ car-price/src/reports/reports.controller.ts
@@ -24,7 +24,10 @@ export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
   @Get()
-  getEstimate(@Query() query: GetEstimateDto) {}
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query);
+  }
+
 
   @Post()
   @UseGuards(AuthGuard)
